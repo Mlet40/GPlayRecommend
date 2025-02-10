04f0883f95ee2064d139feb2a3321d5a9c473b30
fvdias78 <fabiovdias@gmail.com>
2025-02-09 22:22:38 -0300

    init

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -16,3 +16,8 @@ resource "aws_s3_bucket_object" "processed_folder" {
   bucket = aws_s3_bucket.datalake.id
   key    = "processed/"
 }
+
+resource "aws_s3_bucket_object" "raw_folder" {
+  bucket = aws_s3_bucket.datalake.id
+  key    = "gold/"
+}
--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -2,6 +2,8 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.datalake.id
 }
 
+
+
 output "s3_bucket_arn" {
   value = aws_s3_bucket.datalake.arn
 }
