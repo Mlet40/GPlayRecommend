28690e768dcb77613bdc531297a4c7a5f3242c41
fvdias78 <fabiovdias@gmail.com>
2025-02-12 16:42:35 -0300

    teste

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -3,6 +3,7 @@ output "bucket_datalake_name" {
   value       = aws_s3_bucket.datalake.bucket
 }
 
+
 output "ecs_cluster_arn" {
   description = "ARN do ECS Cluster"
   value       = aws_ecs_cluster.this.arn
