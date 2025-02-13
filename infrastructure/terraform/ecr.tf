9a1df8aaaa2d8fe9ec16a24f95da3a432ce3a232
fvdias78 <fabiovdias@gmail.com>
2025-02-13 20:53:58 -0300

    tentativa 4

--- infrastructure/terraform/ecr.tf
+++ infrastructure/terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "featurestoreengine" {
-  name = "featurestoreengine"
+  name = "featurestore"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
