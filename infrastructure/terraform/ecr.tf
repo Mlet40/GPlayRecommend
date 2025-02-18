74889aa733f6e563b8123ef03bdb4355c447804b
fvdias78 <fabiovdias@gmail.com>
2025-02-17 23:49:03 -0300

    tentativa 6

--- infrastructure/terraform/ecr.tf
+++ infrastructure/terraform/ecr.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "globo_recommend" {
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
+
   }
   tags = {
     Environment = "Development"
