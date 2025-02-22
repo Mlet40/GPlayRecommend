2fbaeeea5242e17ff2c3291a51dd7377724e7536
fvdias78 <fabiovdias@gmail.com>
2025-02-22 11:06:28 -0300

    ultima

---
+++ infrastructure/terraform/alb.tf
@@ -0,0 +1,33 @@
+resource "aws_lb" "recommend_api_lb" {
+  name               = "recommend-api-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_sg.id]
+  subnets           = [aws_subnet.public_subnet.id]
+}
+
+resource "aws_lb_target_group" "recommend_api_tg" {
+  name     = "recommend-api-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.this.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.recommend_api_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.recommend_api_tg.arn
+  }
+}
---
+++ infrastructure/terraform/ecs_service.tf
@@ -0,0 +1,22 @@
+resource "aws_ecs_service" "recommend_api_service" {
+  name            = "recommend-api-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.recommend_api_task.arn
+  launch_type     = "FARGATE"
+
+  desired_count   = 1 # Mantém pelo menos 1 Task rodando sempre!
+
+  network_configuration {
+    subnets         = [aws_subnet.public_subnet.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.recommend_api_tg.arn
+    container_name   = "recommend-api-container" #  O mesmo nome definido na Task Definition
+    container_port   = 5000
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
