d288a6f6405ca9adb22c5335463fd2fc43391e86
fvdias78 <fabiovdias@gmail.com>
2025-02-21 23:32:51 -0300

    init new

---
+++ infrastructure/terraform/sg.tf
@@ -0,0 +1,24 @@
+# Criando Security Group para ECS Fargate
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.this.id  # Associação com a VPC
+
+  # Permitir tráfego de entrada na porta 5000
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # ⚠️ Permite acesso público (Ajuste conforme necessário)
+  }
+
+  # Permitir tráfego de saída irrestrito (ECS precisa acessar internet)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "globo-ecs-sg"
+  }
+}
