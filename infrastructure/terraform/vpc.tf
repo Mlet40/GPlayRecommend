79d1558c65460bbad6bd9e70542dd296a8651ba5
fvdias78 <fabiovdias@gmail.com>
2025-02-22 11:27:04 -0300

    ultima

--- infrastructure/terraform/vpc.tf
+++ infrastructure/terraform/vpc.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnet" {
 # Criando Subnet Pública (Para Load Balancer ou Bastion Host)
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
 
