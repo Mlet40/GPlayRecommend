f3c90fb3232ecd1fe71349f396941ee0f5c6832a
fvdias78 <fabiovdias@gmail.com>
2025-02-26 23:48:11 -0300

    inserindo vpc endpoint

--- infrastructure/terraform/vpc.tf
+++ infrastructure/terraform/vpc.tf
@@ -114,18 +114,17 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id       = "aws_vpc.this.id" # Substitua pelo ID da sua VPC
-  service_name   = "com.amazonaws.${var.aws_region}.ecr.api" # Substitua pela sua região
-  vpc_endpoint_type = "Interface"
-  subnet_ids    = aws_subnet.private_subnet.id # Substitua pelo ID da sua sub-rede privada
-  security_group_ids = aws_security_group.ecs_sg.id # Substitua pelo ID do seu grupo de segurança
+  vpc_id             = aws_vpc.this.id
+  service_name       = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = [aws_subnet.private_subnet.id]  # Correção: Colocar em lista
+  security_group_ids = [aws_security_group.ecs_sg.id] # Correção: Colocar em lista
 }
 
-# Endpoint para o ECR DKR
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id       = "aws_vpc.this.id"
-  service_name   = "com.amazonaws.${var.aws_region}.ecr.dkr"
-  vpc_endpoint_type = "Interface"
-  subnet_ids    = aws_subnet.private_subnet.id
-  security_group_ids = aws_security_group.ecs_sg.id
+  vpc_id             = aws_vpc.this.id
+  service_name       = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = [aws_subnet.private_subnet.id]  # Correção: Colocar em lista
+  security_group_ids = [aws_security_group.ecs_sg.id] # Correção: Colocar em lista
 }
