058b3c332e92bca226554f621f6bf0520bc1be86
fvdias78 <fabiovdias@gmail.com>
2025-02-27 17:57:14 -0300

    teste

--- infrastructure/terraform/sg.tf
+++ infrastructure/terraform/sg.tf
@@ -30,3 +30,22 @@ resource "aws_security_group" "ecs_sg" {
     Name = "globo-ecs-sg"
   }
 }
+
+resource "aws_security_group" "ecs_task_sg" {
+  name        = "ecs-task-sg"
+  description = "Security Group para tarefas ECS acessarem o VPC Endpoint"
+  vpc_id      = aws_vpc.this.id
+
+  # Egress: permite que a tarefa se comunique via HTTPS com o VPC Endpoint
+  egress {
+    description     = "Permitir saída HTTPS para o VPC Endpoint"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.vpc_endpoint_sg.id]  # Referência ao SG do VPC Endpoint
+  }
+
+  # Opcionalmente, se sua tarefa precisar receber conexões (ingress), defina regras de entrada adequadas.
+  # Caso contrário, se a comunicação for apenas iniciada pela tarefa, regras de ingress podem não ser necessárias.
+}
+
