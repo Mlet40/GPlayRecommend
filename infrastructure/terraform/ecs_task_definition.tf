24188b415073d154185e737aba6f0799dbe06e9a
fvdias78 <fabiovdias@gmail.com>
2025-02-27 22:09:53 -0300

    turbinando as instancias de treino

--- infrastructure/terraform/ecs_task_definition.tf
+++ infrastructure/terraform/ecs_task_definition.tf
@@ -33,29 +33,36 @@ DEFS
 
 resource "aws_ecs_task_definition" "trainningmodel_task" {
   family                   = "trainning-task"
-  container_definitions    = jsonencode([
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  cpu                      = "4096"    # 4 CPUs
+  memory                   = "16384"   # 16GB de RAM
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
     {
       name      = "trainningmodel-container"
-      image     = "${var.trainning_docker_image}"
-      cpu       = 256
-      memory    = 512
+      image     = var.trainning_docker_image
+      cpu       = 4096      # 4 CPUs para o container
+      memory    = 16384     # 16GB de RAM para o container
       essential = true
       logConfiguration = {
-        logDriver = "awslogs"
+        logDriver = "awslogs",
         options = {
-          "awslogs-group"         = "/ecs/trainningmodel"
-          "awslogs-region"        = "us-east-1"
+          "awslogs-group"         = "/ecs/trainningmodel",
+          "awslogs-region"        = "us-east-1",
           "awslogs-stream-prefix" = "ecs"
         }
-      }
+      },
+      resourceRequirements = [
+        {
+          type  = "GPU",
+          value = "1"
+        }
+      ]
     }
   ])
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  memory                   = "512"
-  cpu                      = "256"
-  execution_role_arn = aws_iam_role.ecs_execution_role.arn
-  task_role_arn       = aws_iam_role.ecs_task_role.arn
 }
 
 
