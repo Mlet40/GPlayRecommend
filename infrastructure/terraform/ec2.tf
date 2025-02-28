28479f5690dde1d37388668750e772f0a87b0e3e
fvdias78 <fabiovdias@gmail.com>
2025-02-28 00:10:58 -0300

    turbinando as instancias de treino

--- infrastructure/terraform/ec2.tf
+++ infrastructure/terraform/ec2.tf
@@ -28,7 +28,7 @@ resource "aws_launch_template" "ecs_gpu" {
 
   user_data = base64encode(<<EOF
 #!/bin/bash
-echo ECS_CLUSTER=${aws_ecs_cluster.cluster.name} >> /etc/ecs/ecs.config
+echo ECS_CLUSTER=${aws_ecs_cluster.this.name} >> /etc/ecs/ecs.config
 EOF
   )
 
