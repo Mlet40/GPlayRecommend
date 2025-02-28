5462a34e206f3a92430cc06579bde953e8aadb9f
fvdias78 <fabiovdias@gmail.com>
2025-02-27 23:35:19 -0300

    turbinando as instancias de treino

---
+++ infrastructure/terraform/iam_ec2_role.tf
@@ -0,0 +1,28 @@
+# Definindo a política de confiança para a instância EC2
+data "aws_iam_policy_document" "ecs_instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# Criando o Role para as instâncias ECS
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "ecsInstanceRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_instance_assume_role_policy.json
+}
+
+# Anexando a política necessária para as instâncias ECS
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+# Criando o Instance Profile para as instâncias ECS
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecsInstanceProfile"
+  role = aws_iam_role.ecs_instance_role.name
+}
