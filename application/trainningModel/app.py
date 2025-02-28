e8aa65cbefc9b5a3a237fc2035b41b83a327e404
fvdias78 <fabiovdias@gmail.com>
2025-02-28 01:24:18 -0300

    turbinando as instancias de treino

--- application/trainningModel/app.py
+++ application/trainningModel/app.py
@@ -6,7 +6,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import boto3
 
-
 # --- CONFIGURAÇÕES DO S3 ---
 print('Configuração do S3')
 bucket_name = "globoplay-datalak"
