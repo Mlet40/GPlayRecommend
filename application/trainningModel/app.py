2ef16edebb8cfe86c017c6dc75ee81162f806229
fvdias78 <fabiovdias@gmail.com>
2025-03-02 16:06:00 -0300

    alterando a api recommend

--- application/trainningModel/app.py
+++ application/trainningModel/app.py
@@ -12,7 +12,51 @@ import math
 # --- CONFIGURAÇÃO DO REDIS ---
 redis_host = "my-redis-cache.ichbon.0001.use1.cache.amazonaws.com"  # Exemplo: "redis.mycompany.cache.amazonaws.com" ou "localhost"
 redis_port = 6379
+redis_client = redis.Redis(host=redis_host, port=redis_port)
 
+# --- CONFIGURAÇÕES DO S3 ---
+print('Configuração do S3')
+bucket_name = "globoplay-datalak"
+# Pasta onde os dados da feature store foram salvos na etapa de ingestão
+input_prefix = "featStore/"
+# Pasta para salvar os dados treinados
+output_prefix = "featStore/trainned/"
+s3 = boto3.client("s3")
+
+# Função para resetar o cache do Redis (somente as chaves de recomendação)
+def reset_recommendations_cache():
+    # Obtém todas as chaves que começam com "recommend:"
+    keys = redis_client.keys("recommend:*")
+    if keys:
+        redis_client.delete(*keys)
+        print(f"Chaves apagadas: {[key.decode('utf-8') for key in keys]}")
+    else:
+        print("Nenhuma chave de recomendação encontrada para apagar.")
+
+# Função para salvar as recomendações no Redis (top 10 por coluna, com page e score)
+def save_recommendations_to_redis(sim_df,df_based):
+    
+    df_based_filtered = df_based[["page","url", "issued", "title"]]
+    df_based_filtered = df_based_filtered.set_index("page")
+    
+    for param in sim_df.columns:
+       
+        sorted_series = sim_df[param].sort_values(ascending=False).head(10)
+
+        df_recommend = sorted_series.reset_index()
+        df_recommend.columns = ["page", "score"]
+        
+        df_recommend['page'] = df_recommend['page'].astype(str)
+        df_based_filtered.index = df_based_filtered.index.astype(str)
+        
+        df_merged = df_recommend.merge(df_based_filtered, left_on="page", right_index=True, how="left")
+
+        # Converte o DataFrame para JSON
+        result_json = df_merged.to_json(orient="records")
+        # Salva no Redis com a chave "recommend:{param}"
+        redis_key = f"recommend:{param}"
+        redis_client.set(redis_key, result_json)
+        print(f"Salvou recomendação para '{param}' em {redis_key}.")
 
 
 def save_dataframe_in_chunks(df, base_key, chunk_size=1000):
@@ -55,15 +99,6 @@ def save_to_s3(df, filename, prefix):
     buffer.seek(0)
     s3.upload_fileobj(buffer, bucket_name, f"{prefix}{filename}")   
 
-# --- CONFIGURAÇÕES DO S3 ---
-print('Configuração do S3')
-bucket_name = "globoplay-datalak"
-# Pasta onde os dados da feature store foram salvos na etapa de ingestão
-input_prefix = "featStore/"
-# Pasta para salvar os dados treinados
-output_prefix = "featStore/trainned/"
-
-s3 = boto3.client("s3")
 
 # --- CARREGAR DADOS PRÉ-PROCESSADOS DA FEATURE STORE ---
 # Supondo que o arquivo tenha sido salvo como "featstore_base.parquet"
@@ -83,21 +118,8 @@ print('Salvando arquivo Parquet no S3')
 save_to_s3(sim_df, "sim_df.parquet",output_prefix)
 
 print('Salvando arquivo Redis')
+print("Resetando o cache de recomendações no Redis...")
+reset_recommendations_cache()
 
-redis_client = redis.Redis(host=redis_host, port=redis_port)
-#save_dataframe_in_chunks(sim_df, "sim_df", chunk_size=1000)
-#salvando no redis
-for param in sim_df.columns:
-    # Ordena os valores da coluna em ordem decrescente e pega os top 10
-    sorted_series = sim_df[param].sort_values(ascending=False).head(10)
-    # Converte a Series para DataFrame, preservando o índice (page) e os valores (score)
-    df_recommend = sorted_series.reset_index()
-    df_recommend.columns = ["page", "score"]
-    # Converte o DataFrame para JSON
-    result_json = df_recommend.to_json(orient="records")
-    # Salva no Redis com a chave "recommend:{param}"
-    redis_key = f"recommend:{param}"
-    redis_client.set(redis_key, result_json)
-    print(f"Salvou recomendação para '{param}' em {redis_key}.")
-
-print("Processo concluído com sucesso.")
+print("Salvando as novas recomendações no Redis...")
+save_recommendations_to_redis(sim_df,df_based)
